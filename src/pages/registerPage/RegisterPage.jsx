5d900eae2cf79863a0ea598e8d8cc37f4f002f7f
Harrison <harrisonderick.65@gmail.com>
2022-08-18 23:44:03 +0100

    chore: customButton and customInput imported into registerPage

--- src/pages/registerPage/RegisterPage.jsx
+++ src/pages/registerPage/RegisterPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import CustomButton from '../../components/customButton/CustomButton';
+import CustomInput from '../../components/customInput/CustomInput';
 function RegisterPage() {
   return (
     <div>RegisterPage</div>
