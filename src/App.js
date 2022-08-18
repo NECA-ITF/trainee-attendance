39c8e6c2b4d0f784bf166c5c83104b897e2594af
Harrison <harrisonderick.65@gmail.com>
2022-08-18 23:22:46 +0100

    chore: register page folder setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import CustomButton from './components/customButton/CustomButton';
 import CustomInput from './components/customInput/CustomInput';
-
 function App() {
   return (
     <div className="App">
