8586a9cf91013037355d7f0483716262392007c3
olaniyiisrael <davdto56@gmail.com>
2022-08-19 10:49:16 +0100

    feat: register background

--- src/pages/registerPage/RegisterPage.jsx
+++ src/pages/registerPage/RegisterPage.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import CustomButton from '../../components/customButton/CustomButton';
 import CustomInput from '../../components/customInput/CustomInput';
+import bg from './images/bg.jpeg'
+import './RegisterPage.css';
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div className='register-page'>
+        <div className='register-container'>
+          <img src={bg} className="pix" alt='bg'/>
+        </div>
+    </div>
   );
 }
 
