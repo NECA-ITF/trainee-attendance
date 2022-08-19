dbfeaedfe7b10584a2bc133340cd7d707d0897ee
Harrison <harrisonderick.65@gmail.com>
2022-08-19 11:00:31 +0100

    --

--- src/pages/registerPage/RegisterPage.jsx
+++ src/pages/registerPage/RegisterPage.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import CustomButton from '../../components/customButton/CustomButton';
 import CustomInput from '../../components/customInput/CustomInput';
+import './RegisterPage.css';
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div><br /><br /><br /><br /><br />
+      <CustomButton title= "Register" onClick={()=> console.log("clicked")} />
+      <CustomInput placeholder="Email" width={200} height={10} borderRadius={50} />
+    </div>
   );
 }
 
