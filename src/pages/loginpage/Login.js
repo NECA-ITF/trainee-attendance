54d71851296cb51820a11cc9d8e5d373a5d27760
unknown <Lazarus B. Umeh>
2022-08-22 15:13:58 +0100

    Added image and Button

--- src/pages/loginpage/Login.js
+++ src/pages/loginpage/Login.js
@@ -1,42 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import CustomButton from '../../components/customButton/CustomButton';
+import CustomInput from '../../components/customInput/CustomInput';
 import './Login.css'
-import CustomButton from '../loginpage/customButton/CustomButton'
-import itf_logo from '../loginpage/itf_logo.png'
-import profile_picture from '../loginpage/profile_picture.png'
-
+import avatar from './images/avatar.jpeg'
+import logo from './images/logo.jpeg'
+import bg from './images/bg.png'
 function Login() {
-    return (
-        <div className='loginPage'>
-            <form action="">
-                
-            <div className='logo'>
-                <h1><img src={itf_logo} alt="itf-logo" id='Itflogo' /><b>INDUSTRIAL TRAINING FUND</b></h1>
-            </div>
+  return (
+      <div className='login-page'>
+        <div className='login-cont'>
+          <div className='login-div'>
+            <form action="" className='frm'>
+              <div className='mainlogo'>
+                <img src={logo} alt="logo" width={80}/>
+                <h2>INDUSTRIAL TRAINING FUND</h2>
+              </div>
+              <div className='avatar'>
+                <img src={avatar} alt="avatar" width={150}/>
+              <h2 style={{ fontWeight:'100'}}>ATTENDANCE SERVICE</h2>
+              </div>
 
-            <div className='profilePicture'>
-                <img src={profile_picture} alt="profile-picture" id='ProfilePicture' />
-                <h2>ATTENDANCE SERVICE</h2><br/>
-            </div>
+              <div className='custom-input'>
+                <CustomInput placeholder="Email Address or Number" className="input" backgroundColor='#EEEEEE' outline='none'/><br />
+                <CustomInput placeholder="Password" className="input" backgroundColor='#EEEEEE' outline='none'/><br />
+               </div>
+                <br/>
+              <div className='custom-button'>
+                <CustomButton title='Log in' borderRadius={2} width={300} />
+              </div>
+              <br/>
 
-            {/* // Maryam section */}
-            {/* <div class="emailInput"> */}
-                
-            {/* <div class="passwordInput"> */}
-             
-
-            <div className='login'>
-                <CustomButton title="Log In"/>
-            </div><br/>
-
-            <p id='forgotPassword' >Forgotten password ?</p><br/>
-
-            <hr/><br/>
-
-            <CustomButton title ="Create New Page"/>
-            
+              <h4>Forgot Password? <span></span></h4>
+              <div className='line'></div>
+              <div>
+              <CustomButton title='create account' borderRadius={8} width={200} backgroundColor='green' />
+              </div>
             </form>
+          </div>
+
+
+
+
+
+            <img id='image' src={bg} className="pix" alt='bg' />
         </div>
-    )
+      </div>
+
+
+  );
 }
 
-export default Login
+export default Login;
