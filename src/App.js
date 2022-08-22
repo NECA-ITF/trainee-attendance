388a93d9b96618eb9129008f06f529310d43748a
MacBook <macbookair@MacBooks-MacBook-Air.local>
2022-08-22 13:59:37 +0100

    waiting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-<<<<<<< HEAD
-// import CustomButton from './components/customButton/CustomButton';
-// import CustomInput from './components/customInput/CustomInput';
-import Dashboard from './pages/dashboardPage/Dashboard'
-=======
 import Dashboard from './pages/dashboardPage/Dashboard';
 
->>>>>>> dashboard
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        {/* <CustomButton title= "Register" onClick={()=> console.log("clicked")}/>
-        <CustomInput placeholder= "Email"/> */}
-=======
->>>>>>> dashboard
         <Dashboard />
     </div>
   );
--- src/pages/dashboardPage/Dashboard.jsx
+++ src/pages/dashboardPage/Dashboard.jsx
@@ -1,11 +1,13 @@
 import './Dashboard.css'
 import React from 'react'
 import AttendanceItem from './AttendanceItem'
+import AttendanceSlider from './AttendanceSlider'
 
 function Dashboard() {
   return (
-    <div>
+    <div className = 'dashboardContainer'>
       <AttendanceItem />
+      <AttendanceSlider />
     </div>
   )
 }
