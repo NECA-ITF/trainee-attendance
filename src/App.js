21b7069643481ff06b7f6f0dd8cab71c409234d7
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-08-22 10:17:13 +0100

    added the resource files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-// import CustomButton from './components/customButton/CustomButton';
-// import CustomInput from './components/customInput/CustomInput';
+import Dashboard from './pages/dashboardPage/Dashboard';
+
 
 function App() {
   return (
     <div className="App">
-        {/* <CustomButton title= "Register" onClick={()=> console.log("clicked")}/>
-        <CustomInput placeholder= "Email"/> */}
+        <Dashboard />
     </div>
   );
 }
