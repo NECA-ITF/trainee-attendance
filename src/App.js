b005a1228ab96fa41cf929f1f7544e1ab268aa34
jothamardel <ardelmbiplang@gmail.com>
2022-08-23 22:08:27 +0100

    chore: updating application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Dashboard from './pages/dashboardPage/Dashboard';
+import Homepage from './pages/homepage/Homepage';
+import Login from './pages/loginpage/LoginPage';
+import WaitingPage from './pages/waitingPage/WaitingPage';
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-  
-
+      <Login />
     </div>
   );
 }
