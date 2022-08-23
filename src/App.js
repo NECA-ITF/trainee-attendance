101ec623702b9ca2f003dba4bfab535bac8c7515
Harrison <harrisonderick.65@gmail.com>
2022-08-23 16:08:09 +0100

    chore: registerPage updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import RegisterPage from './pages/registerPage/RegisterPage';
-// import CustomButton from './components/customButton/CustomButton';
-// import CustomInput from './components/customInput/CustomInput';
 
 function App() {
   return (
     <div className="App">
-        {/* <CustomButton title= "Register" onClick={()=> console.log("clicked")}/>
-        <CustomInput placeholder= "Email"/> */}
-        <RegisterPage/>
+
     </div>
   );
 }
