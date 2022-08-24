7dae25b35cca7f7a82078d0bfcdf61e66c88666e
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-08-24 16:24:21 +0100

    chore: finished the rest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import HomePage from './pages/homepage/Homepage'
+// import HomePage from './pages/homepage/Homepage'
+import LoginPage from  './pages/loginpage/LoginPage'
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      {/* <HomePage/> */}
+      <LoginPage />
+
     </div>
   );
 }
