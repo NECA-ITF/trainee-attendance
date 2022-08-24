f8ba167ea76defbc4a28fdd02a30a9a1ba692475
Hameedarh <ajibadeolayimika@gmail.com>
2022-08-24 21:17:30 +0100

    merging

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
 import HomePage from './pages/homePage/Homepage'
-import ProfilePage from './pages/profilePage/ProfilePage'
-import LoginPage from './pages/loginPage/LoginPage'
-import RegisterPage from './pages/registerPage/RegisterPage';
-import WaitingPage from './pages/waitingPage/WaitingPage'
 function App() {
   return (
     <div className="App">
-      {/* <HomePage/> */}
-      <ProfilePage/>
-      {/* <Dashboard/> */}
-      {/* <RegisterPage/> */}
-      {/* <LoginPage/> */}
-      {/* <WaitingPage/> */}
+      <HomePage/>
     </div>
   );
 }
