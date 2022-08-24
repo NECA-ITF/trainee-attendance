4c2b7cefe1ac7171f30b4c2261c65126b602e600
Harrison <harrisonderick.65@gmail.com>
2022-08-24 18:01:26 +0100

    chore: effecting changes from master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import HomePage from './pages/homepage/Homepage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/HomePage';
+import Dashboard from './pages/DashboardPage/DashboardPage';
+import ProfilePage from './pages/profilePage/ProfilePage';
+// import Login from './pages/registerPage/RegisterPage';
+import RegisterPage from './pages/registerPage/RegisterPage';
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+{/* <RegisterPage /> */}
     </div>
   );
 }
