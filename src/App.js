96dd210f5972ff8da622e17a4c6fbc1614fca8d5
Harrison <harrisonderick.65@gmail.com>
2022-08-25 12:03:09 +0100

    chore: master cleaned up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
-import HomePage from './pages/homepage/Homepage';
+import HomePage from './pages/homePage/HomePage';
 import Dashboard from './pages/dashboardPage/Dashboard';
 import ProfilePage from './pages/profilePage/ProfilePage';
 import EditProfile from './components/EditProfile/EditProfile';
-import Login from './pages/loginpage/LoginPage';
+import Login from './pages/loginPage/LoginPage';
 import RegisterPage from './pages/registerPage/RegisterPage';
 import WaitingPage from './pages/waitingPage/WaitingPage';
 function App() {
